88fa651b93cb7233970bc764aad94efd32e52832
hshehabu <hamzashehabu457@gmail.com>
2025-06-23 16:42:39 +0300

    update testimonials

--- components/testimonial.js
+++ components/testimonial.js
@@ -17,6 +17,21 @@ class Testimonial extends HTMLElement {
                             <div class="grid">
                                 <div class="quote">
                                     <i class="fi flaticon-quote"></i>
+                                    <p>"OGP provided us with highly qualified offshore personnel for our Bluestream projects. Their expertise with work permits and commitment to safety made a real difference. We value their professionalism and reliability as a partner in the Netherlands."</p>
+                                </div>
+                                <div class="client-info">
+                                    <div class="img-holder">
+                                        <img src="assets/images/testimonials/img-3.jpg" alt>
+                                    </div>
+                                    <div class="details">
+                                        <h5>Fecco</h5>
+                                        <p>Project Manager, Bluestream Offshore B.V.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="grid">
+                                <div class="quote">
+                                    <i class="fi flaticon-quote"></i>
                                     <p>"OGP has been instrumental in providing highly qualified personnel for our offshore drilling operations. Their attention to certification requirements and safety standards has been exceptional. The team's responsiveness and professionalism have made them our go-to partner for personnel solutions."</p>
                                 </div>
                                 <div class="client-info">
@@ -25,7 +40,7 @@ class Testimonial extends HTMLElement {
                                     </div>
                                     <div class="details">
                                         <h5>John Anderson</h5>
-                                        <p>Operations Director, Deepwater Drilling</p>
+                                        <p>Operations Director, ExxonMobil</p>
                                     </div>
                                 </div>
                             </div>
@@ -40,22 +55,7 @@ class Testimonial extends HTMLElement {
                                     </div>
                                     <div class="details">
                                         <h5>Michael Chen</h5>
-                                        <p>HR Director, Major Refinery</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="grid">
-                                <div class="quote">
-                                    <i class="fi flaticon-quote"></i>
-                                    <p>"OGP provided us with highly qualified offshore personnel for our Bluestream projects. Their expertise with work permits and commitment to safety made a real difference. We value their professionalism and reliability as a partner in the Netherlands."</p>
-                                </div>
-                                <div class="client-info">
-                                    <div class="img-holder">
-                                        <img src="assets/images/testimonials/img-3.jpg" alt>
-                                    </div>
-                                    <div class="details">
-                                        <h5>Fecco</h5>
-                                        <p>Project Manager, Bluestream Offshore B.V.</p>
+                                        <p>HR Director, Shell</p>
                                     </div>
                                 </div>
                             </div>
